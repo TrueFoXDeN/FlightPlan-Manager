b9afc447137de2bc67af54d5e415b26ade02e034
FoX <laurenz.dilba@hotmail.de>
2020-02-02 00:02:30 +0100

    Beautiyfied the search bar

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function hideButton(){
+    document.getElementById('id_create_flightplan').style.display="none";
+}
+
+function showButton(){
+    document.getElementById('id_create_flightplan').style.display="inline-block";
+}
